4d9cd39b4d0c2958083b8a392345b14ad8b30a44
Jan Piskvor Martinec <github@piskvor.org>
2016-12-15 12:43:52 +0100

    calculate subresource integrity (SRI)

---
+++ sri.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+HASH=$(openssl dgst -sha384 -binary "$1" | openssl base64 -A)
+
+echo -n '<script src="'
+echo -n $(basename $1)
+echo -n '" integrity="sha384-'
+echo -n $HASH
+echo '" crossorigin="anonymous"></script>'
