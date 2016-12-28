eb3e9cd196b3dc1581766c40327b4ce2ea16358f
Jan Piskvor Martinec <github@piskvor.org>
2016-12-28 10:14:59 +0100

    filter sources by input

--- pactl-default-axago.sh
+++ pactl-default-axago.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-DEFAULT_SOURCE=$(pactl list short sources | grep 'C-Media_USB' | cut -f1)
-DEFAULT_SINK=$(pactl list short sinks | grep 'C-Media_USB' | cut -f1)
+DEFAULT_SOURCE=$(pactl list short sources | grep input | grep 'C-Media_USB' | cut -f1)
+if [ $? -gt 0 ]; then
+	exit 1
+fi
+DEFAULT_SINK=$(pactl list short sinks |grep output | grep 'C-Media_USB' | cut -f1)
+if [ $? -gt 0 ]; then
+	exit 1
+fi
 
-if [ "$DEFAULT_SOURCE" -gt 0 ]; then
+if [ "${DEFAULT_SOURCE}" -ge 0 ]; then
 	pactl set-default-source $DEFAULT_SOURCE
 fi
-if [ "$DEFAULT_SINK" -gt 0 ]; then
+if [ "${DEFAULT_SINK}" -ge 0 ]; then
 	pactl set-default-sink $DEFAULT_SINK
 fi
