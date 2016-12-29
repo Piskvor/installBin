aa2bb40fa7c01156d912b79c607b336dedbb0f30
Jan Piskvor Martinec <github@piskvor.org>
2016-12-29 23:04:09 +0100

    JOSM updatable with --all

--- update-josm-latest.sh
+++ update-josm-latest.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+ALL=0
+if [ "$1" = "--all" ]; then
+	ALL=1
+fi
+
 cd `dirname $0`
 wget --timestamping https://josm.openstreetmap.de/josm-latest.jar
+if [ "$ALL" -gt 0 ]; then
+	wget --timestamping https://josm.openstreetmap.de/josm-tested.jar
+fi
 exit $?
 
