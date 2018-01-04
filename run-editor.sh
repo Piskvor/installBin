82d29f884588b5282ac5ab373fe005640220ee5a
Jan Piskvor Martinec <github@piskvor.org>
2018-01-04 10:48:32 +0100

    phpstorm integration

--- run-editor.sh
+++ run-editor.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
-
 url="$1"
-REWRITE=( $(IFS=" " echo "$(echo $url | sed -s 's/%2F/\//g' | sed -s 's/.*file=\(\/.*\)\&line=\(.*\)/\1\ \2/')") );
+REWRITE=( $(IFS=" " echo "$(echo $url | sed -s 's/%2F/\//g;s~file://~file=~g' | sed -s 's/.*file=\(.*\)\&line=\(.*\)/\1\ \2/')") );
 
 line=${REWRITE[1]}
 file=${REWRITE[0]}
 
-# Netbeans
-#netbeans "$file:$line"
-# Kate
-#kate --line $line "$file"
 # Vim
 #vim "$file" +$line
-# Gedit
-#gedit +$line "$file"
-# Komodo
-#komodo "$file#$line"
 # PhpStorm
-#zenity --info --text "~/bin/pstorm --line $line \"$file\""
-~/bin/pstorm --line $line "$file"
+project_dir=$(realpath $(readlink "$HOME/.project"))
+~/bin/pstorm --line $line "$project_dir/$file"
 # Sublime Text 2
 #subl "$file:$line"
