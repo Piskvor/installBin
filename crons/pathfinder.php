702965d61b4cb7fde49fa27adf7323e306d8d999
Jan Piskvor Martinec <github@piskvor.org>
2018-01-31 14:46:48 +0100

    parameters via HTTP

--- crons/pathfinder.php
+++ crons/pathfinder.php
@@ -1,4 +1,3 @@
-#!/usr/bin/php
 <?php
 
 use Pathfinder\Pathfinder;
@@ -14,11 +13,57 @@ $config_defaults = array(
     'departure_time' => 'now',
     'traffic_model' => 'pessimistic',
 );
+$auto_notify = true;
 
 require_once __DIR__.DIRECTORY_SEPARATOR.'pathfinder.config.php';
 require_once __DIR__.DIRECTORY_SEPARATOR.'Pathfinder.class.php';
 
-$selected_config = @$argv[1];
+if (PHP_SAPI === 'cli') {
+    $selected_config = @$argv[1];
+} else {
+    $selected_config = @$_REQUEST['route'];
+    if (!$selected_config) {
+        $selected_config = @$_SERVER['QUERY_STRING'];
+    }
+    $format = @$_REQUEST['format'];
+    if (!$format) {
+        $format = @$_SERVER['HTTP_ACCEPT'];
+    }
+    if (!$format) {
+        $format = 'text/html';
+    }
+
+    if (isset($_REQUEST['notify'])) {
+        $auto_notify = (bool)$_REQUEST['notify'];
+    }
+
+    if ($format === 'application/json') {
+        function formatter($result)
+        {
+            unset($result['command']);
+            header('Content-Type: application/json');
+
+            return json_encode($result);
+        }
+    } elseif ($format === 'text/plain') {
+        function formatter($result)
+        {
+            return $result['text'];
+        }
+    } else {
+        function formatter($result)
+        {
+            return '<a href="'.$result['url'].'">'.$result['text'].'</a>';
+        }
+    }
+}
+
+if (!function_exists('formatter')) {
+    function formatter($result)
+    {
+        return $result['text']."\n";
+    }
+}
 
 $pf = new Pathfinder(
     $config_defaults,
@@ -29,8 +74,8 @@ $pf = new Pathfinder(
         'pushjet_secret' => $pushjet_secret,
         'PUSHJET' => $PUSHJET,
         'demo_data' => $demo_data,
-    )
-
+    ),
+    $auto_notify
 );
 if (!$pf->find($selected_config)) {
     echo implode("\n", $pf->getErrors())."\n";
@@ -44,4 +89,4 @@ if (!$pf->find($selected_config)) {
 
 $result = $pf->getResult();
 
-echo $result['text'] , "\n";
+echo formatter($result);
