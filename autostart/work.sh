78cd2749d9da857aa413506fb2e09673c27667ef
Jan Piskvor Martinec <github@piskvor.org>
2018-02-05 13:57:45 +0100

    show watcher results explicitly

--- autostart/work.sh
+++ autostart/work.sh
@@ -8,4 +8,7 @@ echo ''>$HOME/.project/.php_cs.cache
 cd /home/honza/bin/autostart/dockerized && \
     docker-compose up
 
-
+(
+/home/honza/.project_dev/watcher-in-the-water.sh --watch --symbols 3
+/home/honza/.project_dev/watcher-in-the-water.sh --watch 7 --no-parse
+) &
