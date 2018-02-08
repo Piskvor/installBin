1391641da74fc8bb5e0744797583cb799fcc19b4
Jan Piskvor Martinec <github@piskvor.org>
2018-02-08 15:16:22 +0100

    not in swarm, actually

--- autostart/work.sh
+++ autostart/work.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 /usr/bin/udisksctl mount -b /dev/sdb2 --no-user-interaction || true
 echo ''>$HOME/.project/.php_cs.cache
 
-docker swarm init --advertise-addr 10.42.0.1 || true
+#docker swarm init --advertise-addr 10.42.0.1 || true
 cd /home/honza/bin/autostart/dockerized/autossh && \
  docker build . --tag piskvor-docker-autossh
 
