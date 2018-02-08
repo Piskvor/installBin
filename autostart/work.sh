6e01008a3444ef91f7c0ad78f66f987d4252a163
Jan Piskvor Martinec <github@piskvor.org>
2018-02-08 13:31:10 +0100

    docker autossh

--- autostart/work.sh
+++ autostart/work.sh
@@ -5,6 +5,10 @@ set -euxo pipefail
 /usr/bin/udisksctl mount -b /dev/sdb2 --no-user-interaction || true
 echo ''>$HOME/.project/.php_cs.cache
 
+docker swarm init --advertise-addr 10.42.0.1 || true
+cd /home/honza/bin/autostart/dockerized/autossh && \
+ docker build . --tag piskvor-docker-autossh
+
 cd /home/honza/bin/autostart/dockerized && \
     docker-compose up -d &
 
