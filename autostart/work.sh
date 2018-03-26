f0d3af62c303fbe77709a93fe2c93c9867b51e29
Jan Piskvor Martinec <github@piskvor.org>
2018-03-26 10:59:05 +0200

    use the alt IP address

--- autostart/work.sh
+++ autostart/work.sh
@@ -9,7 +9,7 @@ for i in sdb2 sdc1 sdc2 ; do
 done
 echo ''>$HOME/.project/.php_cs.cache
 
-#docker swarm init --advertise-addr 10.42.0.1 || true
+#docker swarm init --advertise-addr 10.31.4.1 || true
 cd /home/honza/bin/autostart/dockerized/autossh && \
  docker build . --tag piskvor-docker-autossh
 
