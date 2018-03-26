1285ed102da0699651df492a809a62eb714a945a
Jan Piskvor Martinec <github@piskvor.org>
2018-03-26 13:13:36 +0200

    do not build own autossh, start VM

--- autostart/work.sh
+++ autostart/work.sh
@@ -7,16 +7,19 @@ setxkbmap -option grp:alt_caps_toggle,grp_led:scroll,terminate:ctrl_alt_bksp,alt
 for i in sdb2 sdc1 sdc2 ; do
 	/usr/bin/udisksctl mount -b /dev/$i --no-user-interaction || true
 done
-echo ''>$HOME/.project/.php_cs.cache
+echo ''>$HOME/.project/.php_cs.cache || true
 
 #docker swarm init --advertise-addr 10.31.4.1 || true
-cd /home/honza/bin/autostart/dockerized/autossh && \
- docker build . --tag piskvor-docker-autossh
+# cd /home/honza/bin/autostart/dockerized/autossh && \
+# docker build . --tag piskvor-docker-autossh
+
+(
+/home/honza/.project_dev/watcher-in-the-water.sh --watch --symbols 3 &
+/home/honza/.project_dev/watcher-in-the-water.sh --watch 7 --no-parse &
+/home/honza/.project_dev/missing_translations.sh
+) &
+
+vboxmanage startvm {cdbd2f92-00f2-4fa7-b2d2-9d1f5e107244} &
 
 cd /home/honza/bin/autostart/dockerized && \
     docker-compose up -d &
-
-(
-/home/honza/.project_dev/watcher-in-the-water.sh --watch --symbols 3
-/home/honza/.project_dev/watcher-in-the-water.sh --watch 7 --no-parse
-) &
