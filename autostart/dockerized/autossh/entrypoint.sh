a191a9d33959bee02c541c566816e5dd83d31134
Jan Piskvor Martinec <github@piskvor.org>
2018-04-12 12:01:48 +0200

    use controlsocks, forward proxy

--- autostart/dockerized/autossh/entrypoint.sh
+++ autostart/dockerized/autossh/entrypoint.sh
@@ -1,24 +1,30 @@
 #!/bin/sh
 
-touch ${SSH_KEY_FILE:=/id_rsa}
-chmod 0400 ${SSH_KEY_FILE:=/id_rsa}
+touch ${SSH_KEY_FILE:=/id_rsa} || true
+chmod 0400 ${SSH_KEY_FILE:=/id_rsa} || true
 
 STRICT_HOSTS_KEY_CHECKING=no
 KNOWN_HOSTS=${SSH_KNOWN_HOSTS:=/known_hosts}
 if [ -f "${KNOWN_HOSTS}" ]; then
-    chmod 0400 ${KNOWN_HOSTS}
+    chmod 0400 ${KNOWN_HOSTS} || true
     KNOWN_HOSTS_ARG="-o UserKnownHostsFile=${KNOWN_HOSTS}"
     STRICT_HOSTS_KEY_CHECKING=yes
 fi
 
 CONFIG_FILE=${SSH_CONFIG_FILE:=/ssh_config}
 if [ -f "${CONFIG_FILE}" ]; then
-    chmod 0400 ${CONFIG_FILE}
+    chmod 0400 ${CONFIG_FILE} || true
     CONFIG_FILE_ARG="-F ${CONFIG_FILE}"
 fi
 
+SSH_CONTROL_PATH_ARG=""
+SSH_CONTROL_PATH_EXISTS=[ -e ${SSH_CONTROL_PATH:=} ];
+if [ "${SSH_CONTROL_PATH:=}" != "" ]; then
+    SSH_CONTROL_PATH_ARG="-o ControlPath=${SSH_CONTROL_PATH}"
+fi
+
 if [ ! -z "${SSH_HOSTPORT:=}" ]; then
-    HOSTPORT_ARG="-p ${SSH_HOSTPORT}"
+    HOST_PORT_ARG="-p ${SSH_HOSTPORT}"
 fi
 
 # Pick a random port above 32768
@@ -27,20 +33,21 @@ let "DEFAULT_PORT += 32768"
 echo [INFO] Tunneling ${SSH_HOSTUSER:=root}@${SSH_HOSTNAME:=localhost}:${SSH_TUNNEL_REMOTE:=${DEFAULT_PORT}} to ${SSH_TUNNEL_HOST=localhost}:${SSH_TUNNEL_LOCAL:=22}
 
 echo autossh \
- ${CONFIG_FILE_ARG:=} \
  -M 0 \
  -N \
+ ${CONFIG_FILE_ARG:=} \
  -o StrictHostKeyChecking=${STRICT_HOSTS_KEY_CHECKING} ${KNOWN_HOSTS_ARG:=} \
  -o ServerAliveInterval=5 \
  -o ServerAliveCountMax=1 \
  -o IdentitiesOnly=yes \
  -o BatchMode=yes \
  -o VisualHostKey=yes \
+ ${SSH_CONTROL_PATH_ARG:=} \
+ ${SSH_EXTRA_OPTIONS:=} \
  -t -t \
  -i ${SSH_KEY_FILE:=/id_rsa} \
  ${SSH_MODE:=-R} ${SSH_TUNNEL_REMOTE}:${SSH_TUNNEL_HOST}:${SSH_TUNNEL_LOCAL} \
- ${HOSTPORT_ARG:=} \
- ${SSH_EXTRA_OPTIONS:=} \
+ ${HOST_PORT_ARG:=} \
  ${SSH_HOSTUSER}@${SSH_HOSTNAME}
 
 AUTOSSH_PIDFILE=/autossh.pid \
@@ -57,9 +64,15 @@ autossh \
  -o IdentitiesOnly=yes \
  -o BatchMode=yes \
  -o VisualHostKey=yes \
+ ${SSH_CONTROL_PATH_ARG:=} \
+ ${SSH_EXTRA_OPTIONS:=} \
  -t -t \
  -i ${SSH_KEY_FILE:=/id_rsa} \
  ${SSH_MODE:=-R} ${SSH_TUNNEL_REMOTE}:${SSH_TUNNEL_HOST}:${SSH_TUNNEL_LOCAL} \
- ${HOSTPORT_ARG:=} \
- ${SSH_EXTRA_OPTIONS:=} \
- ${SSH_HOSTUSER}@${SSH_HOSTNAME} 2>&1 | ts '[%Y-%m-%d %H:%M:%S]'
+ ${HOST_PORT_ARG:=} \
+ ${SSH_HOSTUSER}@${SSH_HOSTNAME} 2>&1
+
+if [ ! ${SSH_CONTROL_PATH_EXISTS:=} ]; then
+    rm "$SSH_CONTROL_PATH"
+fi
+
