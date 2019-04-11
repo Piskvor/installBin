64f9f0a442d964e4e4f5e4257d57c0070e287f92
Jan Piskvor Martinec <github@piskvor.org>
2019-04-11 11:14:56 +0200

    fix crop ratio, rm templates

--- blurry/screenshot.sh
+++ blurry/screenshot.sh
@@ -15,10 +15,10 @@ import -window root "${TEMP_SCREENSHOT}"
 
 if (identify "${TEMP_SCREENSHOT}" | grep 3000x1920) ; then
     convert "${TEMP_SCREENSHOT}" -gravity East -chop 1920x0 "${TEMP_SCREENSHOT_TEMPLATE}-1${SCREENSHOT_POSTFIX}"
-    convert "${TEMP_SCREENSHOT}" -gravity SouthWest -chop 1080x1080 "${TEMP_SCREENSHOT_TEMPLATE}-2${SCREENSHOT_POSTFIX}"
+    convert "${TEMP_SCREENSHOT}" -gravity SouthWest -chop 1080x840 "${TEMP_SCREENSHOT_TEMPLATE}-2${SCREENSHOT_POSTFIX}"
 fi
 for i in ${TEMP_SCREENSHOT_TEMPLATE}* ; do
     j="$(echo ${i} | sed 's/sharp/out/')"
     convert "${i}" -blur 0x5 "$j"
 done
-#shred -u "${TEMP_SCREENSHOT}"
+rm ${TEMP_SCREENSHOT_TEMPLATE}*
