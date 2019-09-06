0bb53834f35cb9751ab61b597bca98d4109acd54
Jan Piskvor Martinec <github@piskvor.org>
2019-09-06 17:34:16 +0200

    evrouter shellcheck

--- autostart/evrouter-loop.sh
+++ autostart/evrouter-loop.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 WATCHFILE=/run/shm/pluggedKB
-touch $WATCHFILE
+touch "$WATCHFILE"
 
-INOTIFYWAIT=$(which inotifywait)
-RUN_COMMAND=$HOME/bin/evr-start
+INOTIFYWAIT=$(command -v inotifywait)
+RUN_COMMAND="$HOME/bin/evr-start"
 if [ ! -x "$INOTIFYWAIT" ]; then
 	echo "No inotifywait!"
 	exit 2
 fi
 
 while [ -f "$WATCHFILE" ]; do
-	${RUN_COMMAND} $*
+	${RUN_COMMAND} "$@"
 	# wait for stuff to happen
 	${INOTIFYWAIT} -e open "$WATCHFILE"
 	sleep 1
